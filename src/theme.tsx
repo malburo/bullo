48926d8d5952f4a01d31524a522009a9e3dd0f49
malburo <tongquocbao0605@gmail.com>
2021-02-03 18:23:39 +0700

    config theme

---
+++ src/theme.tsx
@@ -0,0 +1,38 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#2F80ED",
+    },
+  },
+  overrides: {
+    MuiButton: {
+      contained: {
+        borderRadius: 8,
+        fontSize: 12,
+        padding: "7px 20px",
+        textTransform: "none",
+      },
+      containedPrimary: {
+        background: "#2F80ED",
+        color: "white",
+        "&:hover": {
+          backgroundColor: "#2d79e1",
+        },
+      },
+      textSecondary: {
+        background: "#F2F2F2",
+        color: "#828282",
+        "&:hover": {
+          backgroundColor: "#e7e7e7",
+        },
+      },
+    },
+  },
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+  },
+});
+
+export default theme;
