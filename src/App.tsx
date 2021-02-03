c3f2a17bd376ca8d058473507212757177bf2501
malburo <tongquocbao0605@gmail.com>
2021-02-03 18:21:47 +0700

    init source

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,39 @@
-function App() {
-  return <div></div>;
-}
+import Header from "./components/Header";
+import { Route, Switch } from "react-router-dom";
+import BoardFeature from "./features/Boards";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Header />
+      <Switch>
+        <Route path="/" component={BoardFeature} />
+      </Switch>
+    </ApolloProvider>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "@material-ui/core";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
