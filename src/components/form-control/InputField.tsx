8155691f541af5000023cb6442f9f8df32318855
malburo <tongquocbao0605@gmail.com>
2021-02-10 18:20:23 +0700

    update add icon inputField.tsx

--- src/components/form-control/InputField.tsx
+++ src/components/form-control/InputField.tsx
@@ -1,29 +1,60 @@
-import { TextField } from "@material-ui/core";
+import {
+  FormControl,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@material-ui/core";
 import { Controller } from "react-hook-form";
 
-interface Props {
+interface InputFieldProps {
   form: any;
   name: string;
   label?: string;
   disabled?: boolean;
+  placeholder?: string;
+  startIcon?: any;
+  endIcon?: any;
+  size?: string;
 }
-const InputField: React.FC<Props> = (props) => {
-  const { form, name, label, disabled } = props;
+
+const InputField: React.FC<InputFieldProps> = (props) => {
+  const {
+    form,
+    name,
+    disabled,
+    placeholder,
+    startIcon,
+    label,
+    size,
+    endIcon,
+  } = props;
   const { errors } = form;
   const hasError = !!errors[name];
   return (
-    <Controller
-      as={<TextField />}
-      control={form.control}
-      name={name}
-      label={label}
-      disabled={disabled}
-      fullWidth
-      variant="outlined"
-      margin="normal"
-      error={hasError}
-      helperText={errors[name]?.message}
-    />
+    <FormControl fullWidth margin="normal" error={hasError}>
+      <Typography variant="h6" color="initial">
+        {label}
+      </Typography>
+      <Controller
+        name={name}
+        control={form.control}
+        as={<TextField />}
+        variant="outlined"
+        error={hasError}
+        disabled={disabled}
+        placeholder={placeholder}
+        size={size}
+        InputProps={{
+          startAdornment: startIcon && (
+            <InputAdornment position="start">{startIcon}</InputAdornment>
+          ),
+          endAdornment: endIcon && (
+            <InputAdornment position="end">{endIcon}</InputAdornment>
+          ),
+        }}
+        helperText={errors[name]?.message}
+      />
+    </FormControl>
   );
 };
 
---
+++ src/components/form-control/PasswordField.tsx
@@ -0,0 +1,60 @@
+import { FormHelperText, InputAdornment } from "@material-ui/core";
+import FormControl from "@material-ui/core/FormControl";
+import IconButton from "@material-ui/core/IconButton";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { useState } from "react";
+import { Controller } from "react-hook-form";
+
+interface PasswordFieldProps {
+  form: any;
+  name: string;
+  label?: string;
+  disabled?: boolean;
+  placeholder?: string;
+  startIcon?: any;
+  size?: string;
+}
+
+const PasswordField: React.FC<PasswordFieldProps> = (props) => {
+  const { form, name, disabled, placeholder, startIcon } = props;
+  const { errors } = form;
+  const hasError = !!errors[name];
+  const [isHidden, setIsHidden] = useState(false);
+
+  const handleClickShowPassword = () => {
+    setIsHidden((prev) => !prev);
+  };
+  return (
+    <FormControl fullWidth variant="outlined" margin="normal" error={hasError}>
+      <Controller
+        name={name}
+        control={form.control}
+        as={<OutlinedInput />}
+        id={name}
+        placeholder={placeholder}
+        type={isHidden ? "text" : "password"}
+        startAdornment={
+          <InputAdornment position="start">{startIcon}</InputAdornment>
+        }
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              edge="end"
+            >
+              {isHidden ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        }
+        error={hasError}
+        disabled={disabled}
+      />
+      <FormHelperText>{errors[name]?.message}</FormHelperText>
+    </FormControl>
+  );
+};
+
+export default PasswordField;
