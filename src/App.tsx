955e93f493f9de2cd35a9decd64f95e7a13c63ad
malburo <tongquocbao0605@gmail.com>
2021-02-10 18:25:18 +0700

    add access_token in header request

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import Header from "./components/Header";
-import { Route, Switch } from "react-router-dom";
-import BoardFeature from "./features/Boards";
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
-import { ApolloProvider } from "@apollo/client";
-
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
+import { Route, Switch } from "react-router-dom";
+import AuthFeature from "./features/Auth";
+import BoardFeature from "./features/Boards";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:8000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem("access_token");
   return {
     headers: {
       ...headers,
@@ -28,8 +31,8 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Header />
       <Switch>
+        <Route path="/auth" component={AuthFeature} />
         <Route path="/" component={BoardFeature} />
       </Switch>
     </ApolloProvider>
